126a386e400c29ac889c5bbb10efbce387d6d659
Paul Lebedev <paul.swan77@gmail.com>
2020-07-10 00:48:53 +0300

    В репозиторий добавлен класс терминальных констант

---
+++ src/ru/lebedev/tm/constant/TerminalConst.java
@@ -0,0 +1,8 @@
+package ru.lebedev.tm.constant;
+
+public class TerminalConst {
+    public static final String HELP = "help";
+    public static final String VERSION = "version";
+    public static final String ABOUT = "about";
+
+}
