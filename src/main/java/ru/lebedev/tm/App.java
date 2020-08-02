cb2c65d70dc6181d981c7384fb8bf4ab033cd125
Paul Lebedev <paul.swan77@gmail.com>
2020-08-02 15:16:27 +0300

    Добавлено отображение новых команд в help.

--- src/main/java/ru/lebedev/tm/App.java
+++ src/main/java/ru/lebedev/tm/App.java
@@ -299,11 +299,20 @@ public class App {
         System.out.println("project-list - Display list of projects.");
         System.out.println("project-create - Create a new project by name.");
         System.out.println("project-clear - Remove all projects.");
+        System.out.println("project-view - Display information about project.");
+        System.out.println("project-remove-by-name - Remove a project by name.");
+        System.out.println("project-remove-by-id - Remove a project by id.");
+        System.out.println("project-remove-by-index - Remove a project by index.");
+        System.out.println("project-update-by-index - Update a project by index.");
         System.out.println();
         System.out.println("task-list - Display list of tasks.");
         System.out.println("task-create - Create a new task by name.");
         System.out.println("task-clear - Remove all tasks.");
-
+        System.out.println("task-view - Display information about task.");
+        System.out.println("task-remove-by-name - Remove a task by name.");
+        System.out.println("task-remove-by-id - Remove a task by id.");
+        System.out.println("task-remove-by-index - Remove a task by index.");
+        System.out.println("task-update-by-index - Update a task by index.");
         return 0;
     }
 
